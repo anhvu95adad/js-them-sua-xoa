03d203d23f0a38e97fc6e7f994dec92fddce6cac
anhvu <vupaph03427@gmail.com>
2018-08-21 17:50:12 +0700

    thêm,xóa user

--- dist/js/common.js
+++ dist/js/common.js
@@ -25,20 +25,25 @@ function getData(){
             // result là cái dữ liệu đã được trả về
             // xử lý ở đây
             //console.log(result);
-            var templatestring = '';
-            if( result && result.length ){ //???
-                for( var i = 0; i < result.length; i++){
+            var templatestring = 0;
+            if( result && result.length ){
+                for( i = 0; i < result.length; i++ )
                     templatestring += `<tr>
-                                            <th>${result[i].first_name}</th>
-                                            <th>${result[i].last_name}</th>
-                                            <th>${result[i].email}</th>
-                                        </tr>
-                                        `;
-                }; //???
-                console.log(templatestring)
-                $('.table-body').append(templatestring);
+                                            <td>${result[i].first_name}</td>
+                                            <td>${result[i].last_name}</td>
+                                            <td>${result[i].email}</td>
+                                            <td>
+                                                <button 
+                                                    type="button" 
+                                                    class="btn btn-danger btn-sm remove-user"
+                                                    user-id="${result[i].id}"
+                                                >
+                                                    <i class="fa fa-remove"></i>
+                                                </button>
+                                            </td>
+                                        </tr>`;
+                                        $('.table-body').append(templatestring);
             }
-
         },
         error: function(err, msg){
             console.log(msg);
@@ -47,3 +52,94 @@ function getData(){
         }
     });
 }
+
+// Create User
+$('#form-action').submit(function(event) {
+    event.preventDefault(); // huỷ action gửi đi của form html
+
+    // khai báo các biến nhận dữ liệu trên form
+    var email     = $('#email').val();
+    var firstName = $('#fist_name').val();
+    var lastName  = $('#last_name').val();
+
+    $.ajax({
+        url: 'http://5b71a13e586eb5001463a75b.mockapi.io/api-vuml/user', // url (demo thôi éo dùng đc đâu)
+        method: "post", // phương thức: get, post, put, delete
+        dataType: "JSON", // kiểu giữ liệu trả về JSON, String, Object, ...
+        data: { // dữ liệu gửi đi tương ứng với key(xem lại postman)
+            first_name: firstName,  
+            email: email,
+            last_name: lastName,
+        },
+        beforeSend: function(){
+            // $('.loading').removeClass('hide');
+            // setInterval(function(){
+            //     $('.loading').toggleClass('txt-red');
+            // }, 200);
+            
+            // Đoạn trong này xử lí action gì đấy trong lúc gửi dữ liệu đi mà chưa có phản hồi
+            // Ví dụ: cho cái icon xoay xoay cho đẹp
+        },
+        complete: function(){
+            // $('.loading').addClass('hide');
+            // Đoạn trong này xử lí action gì đấy sau khi đã có dữ liệu
+            // Ví dụ: tắt cái icon xoay xoay đi
+        },
+        success: function(result){
+            // result là cái dữ liệu đã được trả về
+            // xử lý ở đây
+            //console.log(result);
+            if (result) { // kiểm tra dữ liệu có trả về hay không
+                window.location.replace('file:///D:/training-js/crud-js/starter.html'); // reload về trang chu
+            }
+        },
+        error: function(err, msg){
+            console.log(msg);
+            console.log(err);
+            // nếu lỗi thông báo ở đây
+        }
+    });
+
+});
+
+//delete
+$('.table-body').on('click', '.remove-user',function() {
+    var userID = $(this).attr('user-id');
+    
+    $.ajax({
+        url: `http://5b71a13e586eb5001463a75b.mockapi.io/api-vuml/user/${userID}`, // url (demo thôi éo dùng đc đâu)
+        method: "delete", // phương thức: get, post, put, delete
+        dataType: "JSON", // kiểu giữ liệu trả về JSON, String, Object, ...
+        data: { // dữ liệu gửi đi tương ứng với key(xem lại postman)
+          
+        },
+        beforeSend: function(){
+            // $('.loading').removeClass('hide');
+            // setInterval(function(){
+            //     $('.loading').toggleClass('txt-red');
+            // }, 200);
+            
+            // Đoạn trong này xử lí action gì đấy trong lúc gửi dữ liệu đi mà chưa có phản hồi
+            // Ví dụ: cho cái icon xoay xoay cho đẹp
+        },
+        complete: function(){
+            // $('.loading').addClass('hide');
+            // Đoạn trong này xử lí action gì đấy sau khi đã có dữ liệu
+            // Ví dụ: tắt cái icon xoay xoay đi
+        }, 
+        success: function(result){
+            // result là cái dữ liệu đã được trả về
+            // xử lý ở đây
+            //console.log(result);
+            if (result) { // kiểm tra dữ liệu có trả về hay không
+                window.location.replace('file:///D:/training-js/crud-js/starter.html'); // reload về trang chu
+            }
+        },
+        error: function(err, msg){
+            console.log(msg);
+            console.log(err);
+            // nếu lỗi thông báo ở đây
+        }
+    });
+
+});
